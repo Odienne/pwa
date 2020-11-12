e208baa19d729bcd4b3c229bed972b00a1953fc8
Adam <odienne.adam@gmail.com>
2020-11-12 13:55:34 +0100

    working on sw

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,3 +1,8 @@
+
+self.addEventListener("install", event => {
+    console.log("install successful")
+});
+
 self.addEventListener("fetch", event => {
     console.log("test")
     const requestUrl = new URL(
