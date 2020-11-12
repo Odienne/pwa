1d54b4a56a4732ed4e1e52a5be1b7110c4e386ee
Adam <odienne.adam@gmail.com>
2020-11-12 14:18:39 +0100

    working on sw

--- src/js/main.js
+++ src/js/main.js
@@ -17,38 +17,39 @@ function searchImages() {
     $('.pwa-spinner').show();
     fetch(UNSPLASH_API).then(response => {
         console.log(response.json())
-        console.log(response.blob())
-        console.log(response.body)
-        // Getting a data object from response that contains the necessary data from the server
-        const data = response.json().data;
-        let title = '<p>Aucun résultat pour la recherche <span class="font-weight-bold">"' + search + '"</span></p>';
-        let images = '';
+    }).then(response => {
+            console.log(response.body)
+            // Getting a data object from response that contains the necessary data from the server
+            const data = response.data;
+            let title = '<p>Aucun résultat pour la recherche <span class="font-weight-bold">"' + search + '"</span></p>';
+            let images = '';
 
-        if (data.results.length > 0) {
-            title = '<p>Voici les résultats de la recherche <span class="font-weight-bold">"' + search + '"</span></p>';
+            if (data.results.length > 0) {
+                title = '<p>Voici les résultats de la recherche <span class="font-weight-bold">"' + search + '"</span></p>';
 
-            data.results.map(photo => {
-                let date = new Date(photo.created_at);
-                images += '    <div class="col-md-4">\n' +
-                    '                <div class="card thumbnail mb-4 shadow-sm">\n' +
-                    '                   <img class="img-fluid" src="' + photo.urls.small + '" alt="' + photo.description + '">\n' +
-                    '                    <div class="card-body">\n' +
-                    '                        <p class="card-text ellipsis">' + (photo.description ? photo.description : (photo.alt_description ? photo.alt_description : '')) + '\n' +
-                    '                        </p>\n' +
-                    '                        <div class="d-flex justify-content-between align-items-center">\n' +
-                    '                            <small class="text-muted">' + date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + '</small>\n' +
-                    '                        </div>\n' +
-                    '                    </div>\n' +
-                    '                </div>\n' +
-                    '            </div>\n'
-            })
+                data.results.map(photo => {
+                    let date = new Date(photo.created_at);
+                    images += '    <div class="col-md-4">\n' +
+                        '                <div class="card thumbnail mb-4 shadow-sm">\n' +
+                        '                   <img class="img-fluid" src="' + photo.urls.small + '" alt="' + photo.description + '">\n' +
+                        '                    <div class="card-body">\n' +
+                        '                        <p class="card-text ellipsis">' + (photo.description ? photo.description : (photo.alt_description ? photo.alt_description : '')) + '\n' +
+                        '                        </p>\n' +
+                        '                        <div class="d-flex justify-content-between align-items-center">\n' +
+                        '                            <small class="text-muted">' + date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + '</small>\n' +
+                        '                        </div>\n' +
+                        '                    </div>\n' +
+                        '                </div>\n' +
+                        '            </div>\n'
+                })
+            }
+
+            $("#js-pwa-photos").html(images)
+            $("#js-searchTitle").html(title)
+            $('.pwa-spinner').hide();
+
         }
-
-        $("#js-pwa-photos").html(images)
-        $("#js-searchTitle").html(title)
-        $('.pwa-spinner').hide();
-
-    }).catch(error => console.error('Error while fetching your images', error));
+    ).catch(error => console.error('Error while fetching your images', error));
 
     /*axios.get(UNSPLASH_API)
         .then(response => {
