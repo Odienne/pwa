11941e9be25f243471064b0d3b61fd96e189e4ca
Adam <odienne.adam@gmail.com>
2020-11-12 13:46:13 +0100

    working on sw

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,29 +1,6 @@
 self.addEventListener("fetch", event => {
+    console.log("test")
     const requestUrl = new URL(
         event.request.url
     );
-
-// Si la requête est bien celle que l'on
-// veut simuler
-if (requestUrl.pathname === "/test.html") {
-    // Alors, on modifie la réponse
-    event.respondWith(
-        // Ici je crée une réponse à partir
-        // de rien qui contient uniquement
-        // "Hello Toto"
-        new Response(
-            new Blob(
-                ["Hello Toto"],
-                {type : "text/html"}
-            ),
-            {
-                status: 200,
-                statusText: "OK",
-                headers: {
-                    "Content-Type": "text/html",
-                }
-            }
-        )
-    );
-}
 });
