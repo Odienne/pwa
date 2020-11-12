941ccb1af6e2af5e46d11908d9ddd60781a4cee2
Adam <odienne.adam@gmail.com>
2020-11-12 15:14:08 +0100

    working on sw

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', event => {
+    console.log('install')
+    event.waitUntil(Promise.resolve('Install phase succeed'));
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Claiming control');
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log("fetch")
+    console.log(event)
+});
+
+
+self.addEventListener('message', function(event) {
+    console.log("message")
+    console.log(event)
+});
