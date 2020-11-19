0441f7abd5dc5883581cb350691cffe0ba59412c
Adam <odienne.adam@gmail.com>
2020-11-19 09:30:01 +0100

    working on sw

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 self.addEventListener('install', event => {
     console.log('install')
-/*    event.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll([
-                "https://api.unsplash.com/photo",
-                "https://images.unsplash.com/photo",
-            ]);
-        })
-    );*/
+    /*    event.waitUntil(
+            caches.open('v1').then(function(cache) {
+                return cache.addAll([
+                    "https://api.unsplash.com/photo",
+                    "https://images.unsplash.com/photo",
+                ]);
+            })
+        );*/
 });
 
 self.addEventListener('activate', function (event) {
@@ -47,7 +47,12 @@ self.addEventListener('fetch', function (event) {
                 console.log("réponse fetch", res)
                 return res.json().then((json) => {
                     console.log(json)
-                    const formatted = json.map(item => {
+                    const formatted = {
+                        total: item.total,
+                        total_pages: item.total_pages
+                    }
+
+                    formatted.results = json.results.map(item => {
                         return {
                             alt_description: item.alt_description,
                             color: item.color,
@@ -64,35 +69,35 @@ self.addEventListener('fetch', function (event) {
             })
         )
     }
-/*
-    console.log("fetch")
-    console.log(event)
-    console.log(event.request)
-    let url = event.request.url;
-    let search2 = "https://images.unsplash.com/photo";
-    if (url.includes(search)) { //mise en cache des photos
-        event.respondWith(caches.match(event.request).then(function (response) {
-            // caches.match() fonctionne toujours
-            // mais en cas de succès, la réponse aura une valeur
-            if (response !== undefined) {
-                return response;
-            } else {
-                return fetch(event.request).then(function (response) {
-                    // la réponse ne peut être utilisée qu'une seule fois
-                    // nous devons sauvegarder le clone pour mettre
-                    // une copie en cache et servir le second
-                    let responseClone = response.clone();
-
-                    caches.open('v1').then(function (cache) {
-                        cache.put(event.request, responseClone);
-                    });
+    /*
+        console.log("fetch")
+        console.log(event)
+        console.log(event.request)
+        let url = event.request.url;
+        let search2 = "https://images.unsplash.com/photo";
+        if (url.includes(search)) { //mise en cache des photos
+            event.respondWith(caches.match(event.request).then(function (response) {
+                // caches.match() fonctionne toujours
+                // mais en cas de succès, la réponse aura une valeur
+                if (response !== undefined) {
                     return response;
-                }).catch(function () {
-                    return caches.match('test');
-                });
-            }
-        }));
-    }*/
+                } else {
+                    return fetch(event.request).then(function (response) {
+                        // la réponse ne peut être utilisée qu'une seule fois
+                        // nous devons sauvegarder le clone pour mettre
+                        // une copie en cache et servir le second
+                        let responseClone = response.clone();
+
+                        caches.open('v1').then(function (cache) {
+                            cache.put(event.request, responseClone);
+                        });
+                        return response;
+                    }).catch(function () {
+                        return caches.match('test');
+                    });
+                }
+            }));
+        }*/
 });
 
 
