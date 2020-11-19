13037caa2096a4da03d27746cb33560a3808a9e8
Adam <odienne.adam@gmail.com>
2020-11-19 09:53:49 +0100

    online/offline handling

--- src/js/onlineHandler.js
+++ src/js/onlineHandler.js
@@ -1,8 +1,20 @@
 window.addEventListener('online', event => {
     console.log("online")
+    updateStatus("online")
 });
 
 window.addEventListener('offline', event => {
     console.log("offline")
+    updateStatus("offline")
 });
+
+function updateStatus(msg) {
+    let status = document.getElementById("status");
+    let condition = navigator.onLine ? "ONLINE" : "OFFLINE";
+    status.setAttribute("class", condition);
+    let state = document.getElementById("state");
+    state.innerHTML = condition;
+    let log = document.getElementById("log");
+    log.appendChild(document.createTextNode("Évènement : " + msg + " ; état=" + condition + "\n"));
+}
 
