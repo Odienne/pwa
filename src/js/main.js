764adcf12153d1dd875129149991a019eaba12c3
Adam <odienne.adam@gmail.com>
2020-11-19 16:05:56 +0100

    stockage

--- src/js/main.js
+++ src/js/main.js
@@ -15,41 +15,52 @@ function searchImages() {
     const UNSPLASH_API = 'https://api.unsplash.com/search/photos?client_id=' + KEYS.ACCESS_KEY + endQuery;
 
     //récup donnée stockée en local if hors-ligne
-    if (navigator.onLine) {
+    if (!navigator.onLine) {
         console.log("fetch local data")
-        localforage.setItem('test', 'cc')
+        let localSearchResults = localforage.getItem(search);
+        console.log(localSearchResults);
+        manageAndDisplayData(localSearchResults, search);
+
+    } else {
+        axios.get(UNSPLASH_API)
+            .then(response => {
+                // Getting a data object from response that contains the necessary data from the server
+                const data = response.data;
+                //save search to storage
+                localforage.setItem(search, response.data)
+
+                manageAndDisplayData(data, search);
+
+            }).catch(error => console.error('Error while fetching your images', error));
+    }
+}
+
+function manageAndDisplayData(data, search) {
+    let title = '<p>Aucun résultat pour la recherche <span class="font-weight-bold">"' + search + '"</span></p>';
+    let images = '';
+
+    if (data.results.length > 0) {
+        title = '<p>Voici les résultats de la recherche <span class="font-weight-bold">"' + search + '"</span></p>';
+
+        data.results.map(photo => {
+            let date = new Date(photo.created_at);
+            images += '    <div class="col-md-4">\n' +
+                '                <div class="card thumbnail mb-4 shadow-sm">\n' +
+                '                   <img class="img-fluid" src="' + photo.urls.small + '" alt="' + photo.description + '">\n' +
+                '                    <div class="card-body">\n' +
+                '                        <p class="card-text ellipsis">' + (photo.description ? photo.description : (photo.alt_description ? photo.alt_description : '')) + '\n' +
+                '                        </p>\n' +
+                '                        <div class="d-flex justify-content-between align-items-center">\n' +
+                '                            <small class="text-muted">' + date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + '</small>\n' +
+                '                        </div>\n' +
+                '                    </div>\n' +
+                '                </div>\n' +
+                '            </div>\n'
+        })
     }
 
-    axios.get(UNSPLASH_API)
-        .then(response => {
-            // Getting a data object from response that contains the necessary data from the server
-            const data = response.data;
-            let title = '<p>Aucun résultat pour la recherche <span class="font-weight-bold">"' + search + '"</span></p>';
-            let images = '';
-
-            if (data.results.length > 0) {
-                title = '<p>Voici les résultats de la recherche <span class="font-weight-bold">"' + search + '"</span></p>';
-
-                data.results.map(photo => {
-                    let date = new Date(photo.created_at);
-                    images += '    <div class="col-md-4">\n' +
-                        '                <div class="card thumbnail mb-4 shadow-sm">\n' +
-                        '                   <img class="img-fluid" src="' + photo.urls.small + '" alt="' + photo.description + '">\n' +
-                        '                    <div class="card-body">\n' +
-                        '                        <p class="card-text ellipsis">' + (photo.description ? photo.description : (photo.alt_description ? photo.alt_description : '')) + '\n' +
-                        '                        </p>\n' +
-                        '                        <div class="d-flex justify-content-between align-items-center">\n' +
-                        '                            <small class="text-muted">' + date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + '</small>\n' +
-                        '                        </div>\n' +
-                        '                    </div>\n' +
-                        '                </div>\n' +
-                        '            </div>\n'
-                })
-            }
-
-            $("#js-pwa-photos").html(images)
-            $("#js-searchTitle").html(title)
-            $('.pwa-spinner').hide();
-
-        }).catch(error => console.error('Error while fetching your images', error));
+    $("#js-pwa-photos").html(images)
+    $("#js-searchTitle").html(title)
+    $('.pwa-spinner').hide();
 }
+
