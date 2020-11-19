d2c5f49f91231feea164c7cb2530fbf5c3a1119c
Adam <odienne.adam@gmail.com>
2020-11-19 08:29:12 +0100

    working on sw

--- sw.js
+++ sw.js
@@ -1,21 +1,77 @@
 self.addEventListener('install', event => {
     console.log('install')
-    event.waitUntil(Promise.resolve('Install phase succeed'));
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                "https://api.unsplash.com/photo",
+                "https://images.unsplash.com/photo",
+            ]);
+        })
+    );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     console.log('Claiming control');
     return self.clients.claim();
 });
 
+
 self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).catch(function() {
+            return fetch(event.request).then(function(response) {
+                return caches.open('v1').then(function(cache) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        }).catch(function() {
+            return caches.match('/route-cas-no-match-no-internet');
+        })
+    );
+});
+
+/*self.addEventListener('fetch', function (event) {
     console.log("fetch")
     console.log(event)
     console.log(event.request)
-});
+    let search = "https://api.unsplash.com/photo";
+    if (event.request.url.includes(search)) {
+        event.respondWith()
+    }
+
+    console.log("fetch")
+    console.log(event)
+    console.log(event.request)
+    let url = event.request.url;
+    let search2 = "https://images.unsplash.com/photo";
+    if (url.includes(search)) { //mise en cache des photos
+        event.respondWith(caches.match(event.request).then(function (response) {
+            // caches.match() fonctionne toujours
+            // mais en cas de succès, la réponse aura une valeur
+            if (response !== undefined) {
+                return response;
+            } else {
+                return fetch(event.request).then(function (response) {
+                    // la réponse ne peut être utilisée qu'une seule fois
+                    // nous devons sauvegarder le clone pour mettre
+                    // une copie en cache et servir le second
+                    let responseClone = response.clone();
+
+                    caches.open('v1').then(function (cache) {
+                        cache.put(event.request, responseClone);
+                    });
+                    return response;
+                }).catch(function () {
+                    return caches.match('test');
+                });
+            }
+        }));
+    }
+});*/
 
 
-self.addEventListener('message', function(event) {
+self.addEventListener('message', function (event) {
     console.log("message")
     console.log(event)
 });
