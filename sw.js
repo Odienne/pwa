1794047a98b4bdac3f945a2ae38817230b2fac76
Adam <odienne.adam@gmail.com>
2020-12-09 23:25:03 +0100

    test sync

--- sw.js
+++ sw.js
@@ -175,6 +175,21 @@ self.addEventListener('sync', function(event) {
 function sendData() {
     console.log("back online")
     //send data to api
+    sendMessage("coucou")
+}
+
+function sendMessage(message) {
+    return new Promise(function(resolve, reject) {
+        var messageChannel = new MessageChannel();
+        messageChannel.port1.onmessage = function(event) {
+            if (event.data.error) {
+                reject(event.data.error);
+            } else {
+                resolve(event.data);
+            }
+        };
+        navigator.serviceWorker.controller.postMessage(message, [messageChannel.port2]);
+    });
 }
 
 self.addEventListener('message', function(event){
@@ -182,7 +197,5 @@ self.addEventListener('message', function(event){
 
     console.log("SW Received Message:");
     console.log(data);
-
-    self.test = data.test;
 
 });
